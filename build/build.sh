3a9979110046030b3b083e3ab74704288b8d23ff
Davis Benny <davis.benny@intel.com>
2020-07-09 08:33:24 +0530

    Ubuntu Docker Script for Building Rendezvous-Service
      1. The Docker script is able to build the local copy of source code,
         by mounting the local copy into the container and building the mounted local copy.
    
      2. The script is able to build the latest upstream copy,
         by cloning and building the upstream in /tmp folder of the container and
         finally copying the .war files into the mounted /demo folder of the local copy.
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Copyright 2020 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+
+export http_proxy_host=$(echo $http_proxy | awk -F':' {'print $2'} | tr -d '/')
+export http_proxy_port=$(echo $http_proxy | awk -F':' {'print $3'} | tr -d '/')
+
+export https_proxy_host=$(echo $https_proxy | awk -F':' {'print $2'} | tr -d '/')
+export https_proxy_port=$(echo $https_proxy | awk -F':' {'print $3'} | tr -d '/')
+
+export _JAVA_OPTIONS="-Dhttp.proxyHost=$http_proxy_host -Dhttp.proxyPort=$http_proxy_port -Dhttps.proxyHost=$https_proxy_host -Dhttps.proxyPort=$https_proxy_port"
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+REMOTE_URL=https://github.com/secure-device-onboard/rendezvous-service.git
+REMOTE_BRANCH=master
+
+if [ "$use_remote" = "1" ]; then
+  echo "Building $REMOTE_URL : $REMOTE_BRANCH"
+  cd /tmp/
+  git clone $REMOTE_URL
+  cd /tmp/rendezvous-service/
+  git checkout $REMOTE_BRANCH
+  mvn clean install
+  cd demo && find . -name \*.war -exec cp --parents {} /home/sdouser/rendezvous-service/demo \;
+  cp -r certs/ /home/sdouser/rendezvous-service/demo
+else
+  echo "Building local copy"
+  mvn clean install
+fi
