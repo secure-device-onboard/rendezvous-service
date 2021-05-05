cdaea70a867bda701f5a81d6d6a33bf4e94a6aac
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-05-05 18:28:00 +0530

    Auto-generate JWT signing key
    Currently the default JWT signing key is updated in the properties file.
    In case this is not overridden by the environment variable, all deployed
    units end up using the default keys. This creates a security
    vulnerability where a third-party can pass-on invalid TO1D blobs to the
    device. The overall impact is minimal as the subsequent messages during
    TO2 protocol would stop the third-party gaining access to the device.
    
    The issue is solved by removing the default keys and ensuring the key is
    auto-generated while the service starts.
    
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- demo/cmdDocker.sh
+++ demo/cmdDocker.sh
@@ -6,6 +6,7 @@ SSL_KEY_STORE="rendezvous-keystore.jks"
 JAVA_SSL_PARAMS="-Dserver.ssl.key-store=/home/sdouser/certs/$SSL_KEY_STORE -Dserver.ssl.key-store-password=$SSL_KEY_STORE_PASSWORD"
 JAVA_REDIS_PARAMS="-Dredis.host=$REDIS_HOST -Dredis.password=$REDIS_PASSWORD -Dredis.port=$REDIS_PORT"
 TRUST_STORE_SSL_PARAM="-Djavax.net.ssl.trustStore=/home/sdouser/certs/rendezvous-trusterRootCA.jks -Djavax.net.ssl.trustStorePassword=$SSL_TRUST_STORE_PASSWORD"
+HMAC_SECRET="-Drendezvous.hmacSecret=$(tr -cd 'a-f0-9' < /dev/urandom | head -c128)"
 
 PROXY_SETTINGS=""
 if [ "" != "${http_proxy}" ]
@@ -21,4 +22,4 @@ then
     PROXY_SETTINGS="${PROXY_SETTINGS} -Dhttps.proxyHost=${HTTPS_PROXY_URL} -Dhttps.proxyPort=${HTTPS_PROXY_PORT}"
 fi
 
-java ${PROXY_SETTINGS} ${JAVA_REDIS_PARAMS} ${TRUST_STORE_SSL_PARAM} ${JAVA_SSL_PARAMS} -jar /home/sdouser/rendezvous-service-*.war
+java ${PROXY_SETTINGS} ${JAVA_REDIS_PARAMS} ${TRUST_STORE_SSL_PARAM} ${JAVA_SSL_PARAMS} ${HMAC_SECRET} -jar /home/sdouser/rendezvous-service-*.war
