1f72bd5b2682a3e15842080c7f59400d9beb9719
Tushar Ranjan Behera <tushar.ranjan.behera@intel.com>
2021-05-06 09:41:38 +0530

    Auto-generate JWT signing key (#53)
    Currently the default JWT signing key is updated in the properties file.
    In case this is not overridden by the environment variable, all deployed
    units end up using the default keys. This creates a security
    vulnerability where a third-party can pass-on invalid TO1D blobs to the
    device. The overall impact is minimal as the subsequent messages during
    TO2 protocol would stop the third-party gaining access to the device.
    
    The issue is solved by removing the default keys and ensuring the key is
    auto-generated while the service starts.
    
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- rendezvousService.sh
+++ rendezvousService.sh
@@ -5,6 +5,7 @@
 
 java -Dserver.ssl.key-store=./certs/rendezvous-keystore.jks -Dserver.ssl.key-store-password=123456 \
      -Djavax.net.ssl.trustStore=./certs/rendezvous-trusterRootCA.jks -Djavax.net.ssl.trustStorePassword=123456 \
+     -Drendezvous.hmacSecret=$(tr -cd 'a-f0-9' < /dev/urandom | head -c128) \
      -Dserver.port=8000 -Drendezvous.verificationServiceHost=https://verify.epid-sbx.trustedservices.intel.com \
      -Dspring.profiles.active=production \
      -jar ./target/rendezvous-service-*.war
