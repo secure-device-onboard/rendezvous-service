a6646fe486541392a05deb89852f45b0cc18e81e
Tushar Ranjan Behera <tushar.ranjan.behera@intel.com>
2021-05-12 16:24:57 +0530

    Update comments regarding test key generation (#55)
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- rendezvousService.sh
+++ rendezvousService.sh
@@ -1,11 +1,15 @@
+#!/usr/bin bash
+
 # Copyright 2019 Intel Corporation
 # SPDX-License-Identifier: Apache 2.0
 
-#!/usr/bin bash
+# This is an example implementation. This should be updated in production deployment.
+# Care must be taken to ensure the cryptographic strength of this key.
+HMAC_SECRET="-Drendezvous.hmacSecret=$(openssl rand -hex 64)"
 
 java -Dserver.ssl.key-store=./certs/rendezvous-keystore.jks -Dserver.ssl.key-store-password=123456 \
      -Djavax.net.ssl.trustStore=./certs/rendezvous-trusterRootCA.jks -Djavax.net.ssl.trustStorePassword=123456 \
-     -Drendezvous.hmacSecret=$(tr -cd 'a-f0-9' < /dev/urandom | head -c128) \
+     ${HMAC_SECRET} \
      -Dserver.port=8000 -Drendezvous.verificationServiceHost=https://verify.epid-sbx.trustedservices.intel.com \
      -Dspring.profiles.active=production \
      -jar ./target/rendezvous-service-*.war
